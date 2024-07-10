4cfcbfc1268d692445823ef644fb11cf0ce2f304
sheanemario <sheanemario7@gmail.com>
2024-07-10 22:00:26 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <h1>Hello</h1>
+  )
+}
+export default App
